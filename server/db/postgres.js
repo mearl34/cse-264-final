8e7b657db8a6a0a975e99b1d73c2b48eb1475a89
Anthony Cutrona <ajc327@lehigh.edu>
2026-04-20 10:05:08 -0400

    implemented DB functions

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,35 +1,138 @@
 import pg from 'pg'
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
 const { Client } = pg
- 
+
 const client = new Client({
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DBNAME,
-  user: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.POSTGRES_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
-client.connect()
+await client.connect()
 
-export const query = async (text, values) => {
-    try{
-        const now = new Date()
-        console.log("query to be executed:", text)
-        const res = await client.query(text, values)
-        const now2 = new Date()
-        console.log(`it took ${now2-now}ms to run`)
-        return res
-    } catch (err) {
-        console.error("Problem executing query")
-        console.error(err)
-        throw err
-    }
-}
+export const query = (text, values) => client.query(text, values)
 
 /* 
 HOW TO USE
     query(qs).then(data) => {res.json(data.rows)}
 */
+
+//Add a new user to the database 
+export const addUser = async (gmail, gid, username, pfp, pronouns, is_private) => {
+  const text = `
+    INSERT INTO users (gmail, gid, username, pfp, pronouns, is_private)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING uid, gmail, gid, username, pfp, pronouns, is_private;
+  `
+
+  const values = [gmail, gid, username, pfp, pronouns, is_private]
+
+  const res = await query(text, values)
+  return res.rows[0]
+}
+
+//delete a user by uid
+export const deleteUser = async (uid) => {
+  const text = `
+    DELETE FROM users
+    WHERE uid = $1
+    RETURNING *;
+  `
+  const res = await query(text, [uid])
+  return res.rows[0]
+}
+
+
+//return users when searched for 
+export const searchUsers = async (username) => {
+  const text = `
+    SELECT *
+    FROM users
+    WHERE username ILIKE $1;
+  `
+  const values = [`%${username}%`]
+
+  const res = await query(text, values)
+  return res.rows
+}
+
+//edit a user, replacing all fields with input data
+export const editUser = async (uid, gmail, gid, username, pfp, pronouns, is_private) => {
+  const text = `
+    UPDATE users
+    SET 
+      gmail = $1,
+      gid = $2,
+      username = $3,
+      pfp = $4,
+      pronouns = $5,
+      is_private = $7
+    WHERE uid = $6
+    RETURNING *;
+  `
+
+  const values = [gmail, gid, username, pfp, pronouns, uid, is_private]
+
+  const res = await query(text, values)
+  return res.rows[0]
+}
+
+
+//////////////////////////////////////List entry//////////////////////////////
+
+export const createEntry = async (uid, book_id, status, rating) => {
+  const text = `
+    INSERT INTO list_entries (uid, book_id, status, rating)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `
+
+  const values = [uid, book_id, status, rating]
+
+  const res = await query(text, values)
+  return res.rows[0]
+}
+
+
+export const editEntry = async (uid, book_id, status, rating) => {
+  const text = `
+    UPDATE list_entries
+    SET 
+      status = $1,
+      rating = $2
+    WHERE uid = $3 AND book_id = $4
+    RETURNING *;
+  `
+
+  const values = [status, rating, uid, book_id]
+
+  const res = await query(text, values)
+  return res.rows[0]
+}
+
+
+export const deleteEntry = async (uid, book_id) => {
+  const text = `
+    DELETE FROM list_entries
+    WHERE uid = $1 AND book_id = $2
+    RETURNING *;
+  `
+
+  const res = await query(text, [uid, book_id])
+  return res.rows[0]
+}
+
+
+export const getEntries = async (uid) => {
+  const text = `
+    SELECT *
+    FROM list_entries
+    WHERE uid = $1;
+  `
+
+  const res = await query(text, [uid])
+  return res.rows
+}
