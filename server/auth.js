41c3781df6b702f33dc740b50a354ce1b5fc1bbc
Maisy <mee427@lehigh.edu>
2026-04-22 15:42:42 -0400

    updated book route for searches

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,6 @@
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import express from "express";
+import passport from "passport"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import express from 'express'
 
 const router = express.Router();
 
